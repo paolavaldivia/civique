8e983c3a359983607ec2633afc794581b032274e
Paola Valdivia <paola.valdivia@pigment.com>
2025-12-30 10:13:14 +0100

    Add cloudflare deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
